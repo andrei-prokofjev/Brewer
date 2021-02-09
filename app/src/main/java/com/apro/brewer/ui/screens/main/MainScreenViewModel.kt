250b6c86378dbcab196ac7f1ec399c66ce44ff21
andrei <andrei.prokofjev@gmail.com>
2021-02-09 10:03:16 +0200

    logs removed

--- app/src/main/java/com/apro/brewer/ui/screens/main/MainScreenViewModel.kt
+++ app/src/main/java/com/apro/brewer/ui/screens/main/MainScreenViewModel.kt
@@ -29,7 +29,6 @@ class MainScreenViewModel @Inject constructor(
         mainInteractor.init()
         viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
             mainInteractor.loadBeers().collect {
-                println(">>> post")
                 _beersData.postValue(it)
             }
         }
